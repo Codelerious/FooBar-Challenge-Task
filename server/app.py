e71d50375b3204bcdea793d499f7df40ef2f1d9e
Jothicharan <jothicharanjothibabu@gmail.com>
2024-04-22 18:20:23 +0530

    First commit

---
+++ server/app.py
@@ -0,0 +1,44 @@
+from flask import Flask, request, jsonify # type: ignore
+from flask_cors import CORS, cross_origin # type: ignore
+from openai import AzureOpenAI # type: ignore
+
+app = Flask(__name__)
+cors = CORS(app)
+
+client = AzureOpenAI(
+    api_key="c53afac873314cbf994857f429fbd73f",  
+    api_version="2024-02-01",
+    azure_endpoint = "https://coding-assignment-2.openai.azure.com/"
+    )
+
+@app.route("/")
+def hello_world():
+    return 'This is my first API call!'
+
+@app.route('/post', methods=["POST"])
+def testpost():
+     input_json = request.get_json(force=True) 
+     dictToReturn = {'text':input_json['posting']}
+     return jsonify(dictToReturn)
+
+
+@app.route('/gpt-35', methods=["POST"])
+@cross_origin()
+def getGpt35Response():
+     input_json = request.get_json(force=True) 
+     response = client.chat.completions.create(
+            model="gpt-35-turbo", 
+            messages = [{"role":"system", "content":input_json['query']}])
+     dictToReturn = {'answer':response.choices[0].message.content}
+     return jsonify(dictToReturn)
+
+
+@app.route('/gpt-4', methods=["POST"])
+@cross_origin()
+def getGpt4Response():
+     input_json = request.get_json(force=True) 
+     response = client.chat.completions.create(
+            model="gpt-4-turbo", 
+            messages = [{"role":"system", "content":input_json['query']}])
+     dictToReturn = {'answer':response.choices[0].message.content}
+     return jsonify(dictToReturn)
